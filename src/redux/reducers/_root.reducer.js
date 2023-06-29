6ea63a36adfda171fd6e7b0126f6b87b30861131
Ismail Ali <ishaliprime@gmail.com>
2023-06-29 15:19:23 -0500

    added the cart reducer to the root reducers file

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import meatReducer from './meat.reducer';
+import cart from './cart.reducer'
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -11,8 +12,9 @@ import meatReducer from './meat.reducer';
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
-  user,
-  meatReducer, // will have an id and username if someone is logged in
+  user, // will have an id and username if someone is logged in
+  meatReducer,
+  cart, 
 });
 
 export default rootReducer;
